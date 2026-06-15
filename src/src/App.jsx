d67dcd8f0620001da37a2e6b4e227a4869a563d5
mgsanthiyamdu <mgsanthiya.mdu@gmail.com>
2026-06-16 01:48:52 +0530

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🌿 RhizoSense</h1>
+      <p>Website is Running Successfully 🚀</p>
+    </div>
+  )
+}
